170d3cbebedac620dcd84d0327ba2be5d925e177
Abhi Pathak <abhipathakorg@gmail.com>
2026-06-30 15:29:42 +0530

    LC Questions

---
+++ LeetCodeQuestions/RotateImage.java
@@ -0,0 +1,37 @@
+class RotateImage {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int left=0, right=n-1;
+            while(left<right){
+            int temp=matrix[i][right];
+            matrix[i][right]=matrix[i][left];
+            matrix[i][left]=temp;
+            left++;
+            right--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        RotateImage sol = new RotateImage();
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        sol.rotate(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ LeetCodeQuestions/SpiralMatrix.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int top=0, bottom=n-1;
+        int left=0, right=m-1;
+        
+        List<Integer> result=new ArrayList<>();
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                result.add(matrix[top][i]);
+            }top++;
+            for(int i=top;i<=bottom;i++){
+                result.add(matrix[i][right]);
+            }right--;
+            if (top <= bottom) {
+                for(int i=right;i>=left;i--){
+                    result.add(matrix[bottom][i]);
+                }bottom--;
+            }
+            if (left <= right) {
+                for(int i=bottom;i>=top;i--){
+                    result.add(matrix[i][left]);
+                }left++;
+            }
+        }return result;
+        
+    }
+    public static void main(String[] args) {
+        SpiralMatrix sol = new SpiralMatrix();
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        List<Integer> result = sol.spiralOrder(matrix);
+        System.out.println(result);
+    }
+}
