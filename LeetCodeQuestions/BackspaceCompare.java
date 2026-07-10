3e3f50b9ff7546e2f34f20e2ec49ad90f1c19cbc
Abhi Pathak <abhipathakorg@gmail.com>
2026-07-10 22:36:52 +0530

    LC Ques

---
+++ LeetCodeQuestions/BackspaceCompare.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class BackspaceCompare {
+
+    public static boolean backspaceCompare(String s, String t) {
+
+        Stack<Character> st1 = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c != '#') {
+                st1.push(c);
+            } else if (!st1.isEmpty()) {
+                st1.pop();
+            }
+        }
+
+        for (char c : t.toCharArray()) {
+            if (c != '#') {
+                st2.push(c);
+            } else if (!st2.isEmpty()) {
+                st2.pop();
+            }
+        }
+
+        return st1.equals(st2);
+    }
+
+    public static void main(String[] args) {
+        String s = "ab#c";
+        String t = "ad#c";
+
+        boolean result = backspaceCompare(s, t);
+        System.out.println(result);
+    }
+}
