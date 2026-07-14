03efef4446780ed931d756dde0534ee718870c0f
Abhi Pathak <abhipathakorg@gmail.com>
2026-07-14 21:42:55 +0530

    Stack Ques

---
+++ LeetCodeQuestions/Stack/BasicCalculatorII.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+public class BasicCalculatorII {
+
+    public static int calculate(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int num = 0;
+        char sign = '+';
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
+
+                if (sign == '+') {
+                    stack.push(num);
+                } else if (sign == '-') {
+                    stack.push(-num);
+                } else if (sign == '*') {
+                    stack.push(stack.pop() * num);
+                } else if (sign == '/') {
+                    stack.push(stack.pop() / num);
+                }
+
+                sign = ch;
+                num = 0;
+            }
+        }
+
+        int result = 0;
+
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "3+2*2";
+
+        System.out.println("Answer = " + calculate(s));
+    }
+}
