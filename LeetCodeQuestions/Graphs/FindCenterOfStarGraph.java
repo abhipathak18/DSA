a790bd6e3994cb187fc71b8a8d38110e331dec18
Abhi Pathak <abhipathakorg@gmail.com>
2026-07-14 14:33:47 +0530

    Graph Ques

---
+++ LeetCodeQuestions/Graphs/FindCenterOfStarGraph.java
@@ -0,0 +1,24 @@
+public class FindCenterOfStarGraph {
+
+    public static int findCenter(int[][] edges) {
+
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        }
+
+        return edges[0][1];
+    }
+
+    public static void main(String[] args) {
+
+        int[][] edges = {
+                {1, 2},
+                {2, 3},
+                {4, 2}
+        };
+
+        int center = findCenter(edges);
+
+        System.out.println("Center of Star Graph: " + center);
+    }
+}
