61900532765467b9ec98c73a7a3f1fcc7346b45f
Abhi Pathak <abhipathakorg@gmail.com>
2026-07-20 23:35:24 +0530

    LC Ques

--- LeetCodeQuestions/Shift2DGrid.java
+++ LeetCodeQuestions/Shift2DGrid.java
@@ -10,7 +10,7 @@ class Solution {
 
         for (int i = 0; i  m; i++) {
             for (int j = 0; j < n; j++) {
-                int oldIndex = i * n + j;
+                int oldIndex = i * n + j
                 int newIndex = (oldIndex + k) % total;
 
                 int newRow = newIndex / n;
