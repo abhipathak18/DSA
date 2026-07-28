972eb13a97a7cea6049669b7ae0e73e259aebf9d
Abhi Pathak <abhipathakorg@gmail.com>
2026-07-28 21:14:16 +0530

    Search a 2D Matrix

---
+++ LeetCodeQuestions/Search2DMatrix.java
@@ -0,0 +1,33 @@
+public class Search2DMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Search2DMatrix obj = new Search2DMatrix();
+
+        int[][] matrix = {
+            {1, 4, 7, 11, 15},
+            {2, 5, 8, 12, 19},
+            {3, 6, 9, 16, 22},
+            {10, 13, 14, 17, 24},
+            {18, 21, 23, 26, 30}
+        };
+
+        int target = 5;
+        System.out.println(obj.searchMatrix(matrix, target)); // true
+    }
+}
