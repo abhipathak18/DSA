7fce3e0788048c7f6d180482a12c17efde3f1669
Abhi Pathak <abhipathakorg@gmail.com>
2026-08-05 23:19:11 +0530

    LC Ques 2788

---
+++ LeetCodeQuestions/SplitStringsBySeparator.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplitStringsBySeparator {
+
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> result = new ArrayList<>();
+
+        for (String word : words) {
+            StringBuilder currentWord = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                if (ch == separator) {
+                    if (currentWord.length() > 0) {
+                        result.add(currentWord.toString());
+                        currentWord.setLength(0);
+                    }
+                } else {
+                    currentWord.append(ch);
+                }
+            }
+
+            if (currentWord.length() > 0) {
+                result.add(currentWord.toString());
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SplitStringsBySeparator splitter = new SplitStringsBySeparator();
+
+        List<String> words = List.of("one.two.three", "four.five", "six");
+
+        List<String> result = splitter.splitWordsBySeparator(words, '.');
+
+        System.out.println(result);
+    }
+}
