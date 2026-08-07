fa25b71339bd1b1b8811cc1cc1d9c3f6bfbc6ed9
Abhi Pathak <abhipathakorg@gmail.com>
2026-08-07 22:45:54 +0530

    removed unwanted lines

--- LeetCodeQuestions/FindMissingElements.java
+++ LeetCodeQuestions/FindMissingElements.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class FindMissingElements {
-
     public static List<Integer> findMissingElements(int[] nums) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
@@ -13,9 +12,7 @@ public class FindMissingElements {
             max = Math.max(max, num);
             set.add(num);
         }
-
         List<Integer> ans = new ArrayList<>();
-
         for (int i = min + 1; i < max; i++) {
             if (!set.contains(i)) {
                 ans.add(i);
