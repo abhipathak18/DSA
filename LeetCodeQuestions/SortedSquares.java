c44d156b053882e78b41631238b95c7cd1060ec1
Abhi Pathak <abhipathakorg@gmail.com>
2026-08-10 23:01:09 +0530

    LC Ques 977

---
+++ LeetCodeQuestions/SortedSquares.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SortedSquares {
+
+    public static int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
+        }
+
+        Arrays.sort(nums);
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, 0, 3, 10};
+
+        int[] result = sortedSquares(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
