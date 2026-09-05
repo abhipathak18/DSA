4afa98a53fc4ee80f5f5949a7cf0e091ad3d36cd
Abhi Pathak <abhipathakorg@gmail.com>
2026-09-05 23:21:38 +0530

    LC Ques 99

---
+++ LeetCodeQuestions/Tree/RecoverTree.java
@@ -0,0 +1,56 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class RecoverTree {
+
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = null;
+
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+
+        if (prev != null && prev.val > root.val) {
+            if (first == null) {
+                first = prev;
+            }
+            second = root;
+        }
+
+        prev = root;
+
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        RecoverTree solution = new RecoverTree();
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.right.left = new TreeNode(2);
+
+        solution.recoverTree(root);
+
+        System.out.println("Tree recovered!");
+    }
+}
