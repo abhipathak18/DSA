213d8c27a6b339d3d41709a9eb5728dc7b577ffa
Abhi Pathak <abhipathakorg@gmail.com>
2026-09-17 23:35:57 +0530

    Updated LC Ques 118

--- LeetCodeQuestions/PascalTriangle.java
+++ LeetCodeQuestions/PascalTriangle.java
@@ -4,34 +4,25 @@ public class PascalTriangle {
 
     public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
-
         for (int i = 0; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
-
             row.add(1);
-
             for (int j = 1; j < i; j++) {
                 int value = result.get(i - 1).get(j - 1)
                          + result.get(i - 1).get(j);
-
                 row.add(value);
             }
-
             if (i > 0) {
                 row.add(1);
             }
 
             result.add(row);
         }
-
         return result;
     }
-
     public static void main(String[] args) {
         int numRows = 5;
-
         List<List<Integer>> answer = generate(numRows);
-
         System.out.println(answer);
     }
 }
