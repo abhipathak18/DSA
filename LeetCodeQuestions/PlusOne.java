153eff9db3b78b39009827b6bd2f94da2da27d60
Abhi Pathak <abhipathakorg@gmail.com>
2026-09-17 23:37:33 +0530

    Updated LC Ques 66

--- LeetCodeQuestions/PlusOne.java
+++ LeetCodeQuestions/PlusOne.java
@@ -23,13 +23,10 @@ public class PlusOne {
 
         return result;
     }
-
     public static void main(String[] args) {
 
         int[] digits = {9, 9, 9};
-
         int[] result = plusOne(digits);
-
         System.out.println(Arrays.toString(result));
     }
 }
