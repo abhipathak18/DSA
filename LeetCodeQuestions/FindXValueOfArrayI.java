71e6a1fd8cf3da136a72724251fd5fb1ca3e7bd9
Abhi Pathak <abhipathakorg@gmail.com>
2026-09-21 23:49:07 +0530

    Removed unwanted spaces

--- LeetCodeQuestions/FindXValueOfArrayI.java
+++ LeetCodeQuestions/FindXValueOfArrayI.java
@@ -34,12 +34,9 @@ public class FindXValueOfArrayI {
     }
 
     public static void main(String[] args) {
-
         int[] nums = {1, 2, 3, 4};
         int k = 3;
-
         long[] result = resultArray(nums, k);
-
         System.out.println(Arrays.toString(result));
     }
 }
